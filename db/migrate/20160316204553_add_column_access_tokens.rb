d6e88de78c33dcde131dccc072be2a835dd474cd
RyoKurosaki <ryo.kurosaki@gmail.com>
2016-03-20 07:28:41 +0900

    delete

--- db/migrate/20160316204553_add_column_access_tokens.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnAccessTokens < ActiveRecord::Migration
-  def change
-    create_table :listings do |t|
-      t.string :access_token, :null => false, :limit => 12
-      t.timestamps
-    end
-    add_index :listings, :access_token, :unique => true
-  end
-end
