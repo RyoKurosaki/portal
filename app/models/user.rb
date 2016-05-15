5e0002523fdee1da59c27af6badd8473d7e11f31
Ryo Kurosaki <ryo.kurosaki@gmail.com>
2016-05-15 09:44:23 +0000

    add validation of sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, length: { maximum: 30 }, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "英文字のみが使用できます" }
+  validates :tel, length: { minimum: 10, maximum: 12 }, presence: true, numericality: { only_integer: true }
+  validates :line, format: { with: /\A[a-zA-Z0-9]+\z/i, message: "英数字のみが使用できます" }, allow_blank: true
+  validates :facebook, format: /\A#{URI::regexp(%w(https))}\www.facebook.com/, allow_blank: true
 end
