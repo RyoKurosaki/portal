9c132d6372e7b23df1852de0e185a543eab51c8b
Ryo Kurosaki <ryo.kurosaki@gmail.com>
2016-06-24 15:11:36 +0000

    ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
+  config.force_ssl = true
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
